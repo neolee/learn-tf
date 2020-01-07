92c4bbe4ed4594c616476fe387084ca3aca51277
Neo Lee <neo.lee@gmail.com>
2020-01-07 23:45:42 +0800

    Add 2 basic tutorials

---
+++ test.py
@@ -0,0 +1,7 @@
+from __future__ import absolute_import, division, print_function, unicode_literals
+
+# TensorFlow and tf.keras
+import tensorflow as tf
+from tensorflow import keras
+
+print(tf.__version__)

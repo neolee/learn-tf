8d2a6be4528a54856935c6ebcd9ab212c8a94b9c
Neo Lee <neo.lee@gmail.com>
2020-01-08 19:54:04 +0800

    Better test, more sample, think about GPU

--- test.py
+++ test.py
@@ -4,4 +4,17 @@ from __future__ import absolute_import, division, print_function, unicode_litera
 import tensorflow as tf
 from tensorflow import keras
 
-print(tf.__version__)
+print('TensorFlow version:', tf.__version__)
+print()
+
+devices = tf.config.experimental.list_physical_devices()
+print('Available devices:')
+print(devices)
+print()
+
+print('Device placement sample:')
+tf.debugging.set_log_device_placement(True)
+a = tf.constant([[1.0, 2.0, 3.0], [4.0, 5.0, 6.0]])
+b = tf.constant([[1.0, 2.0], [3.0, 4.0], [5.0, 6.0]])
+c = tf.matmul(a, b)
+print(c)

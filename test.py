9ef32ef0951eef82bc4ae8712dd173fc2246f139
Neo Lee <neo.lee@gmail.com>
2020-01-08 21:47:37 +0800

    Better testing output

--- test.py
+++ test.py
@@ -5,15 +5,19 @@ import tensorflow as tf
 from tensorflow import keras
 
 print('TensorFlow version:', tf.__version__)
+if tf.test.is_gpu_available():
+    print('GPU available')
 print()
 
-devices = tf.config.experimental.list_physical_devices()
-print('Available devices:')
-print(devices)
+cpus = tf.config.experimental.list_physical_devices('CPU')
+print('Number of available CPU:', len(cpus))
+
+gpus = tf.config.experimental.list_physical_devices('GPU')
+print('Number of available GPU:', len(gpus))
 print()
 
-print('Device placement sample:')
 tf.debugging.set_log_device_placement(True)
+print('Device placement log: enabled')
 a = tf.constant([[1.0, 2.0, 3.0], [4.0, 5.0, 6.0]])
 b = tf.constant([[1.0, 2.0], [3.0, 4.0], [5.0, 6.0]])
 c = tf.matmul(a, b)
